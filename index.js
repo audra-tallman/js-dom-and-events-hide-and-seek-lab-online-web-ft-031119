090442ef01326d11caeb8af74565df6c4cad805d
Audra Tallman <audi.tallman@gmail.com>
2019-06-26 01:11:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.querySelector('#nested .target')
 }
+
+function deepestChild() {
+  let node = document.getElementById ('grand-node')
+  let nextNode = node.children[0]
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children [0]
+  }
+  return node
+}
